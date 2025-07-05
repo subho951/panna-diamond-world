a05261b79e288f82acb30db89fd5eb7f7804c7cf
Shubha <shubhadip.sinha@keylines.net>
2025-07-05 14:57:05 +0530

    multi select of upload lead form

---
+++ public/material/backend/assets/js/upload-lead.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        }
+    });
+    $('#telecaller_id').select2({
+        placeholder: "Select Telecaller",
+        allowClear: true
+    });
+    let baseUrl = document.querySelector('meta[name="base-url"]').getAttribute('content');
+    const base_url = document.querySelector('meta[name="baseurl"]').getAttribute('content');
+    // fetch telecallers on branch change 
+    let branch_id;
+    $(document).on('change', '#branch_id', function () {
+        branch_id = $(this).val();
+        if (Number(branch_id)) {
+            $.ajax({
+                url: base_url + '/upload-lead/fetch-telecaller',
+                type: "POST",
+                data: {
+                    branch_id: branch_id,
+                },
+                success: function (res) {
+
+                    $('#telecaller_id').empty();
+                    $('#telecaller_id').append(`
+                  <option value="" selected disabled>Select Telecaller</option>
+                `);
+                    res.forEach(telecaller => {
+                        $('#telecaller_id').append(`
+                    <option value="${telecaller.id}">${telecaller.first_name} ${telecaller.last_name}</option>
+                `);
+                    });
+
+                },
+                error: function (err) {
+                    console.error('Fetch failed:', err);
+                },
+            });
+        }
+    });
+    // fetch telecallers on branch change end
+
+    //fetch campaign on Campaign Type change
+    let campaign_type_id;
+    $(document).on('change', '#campaign_type_id', function () {
+        campaign_type_id = $(this).val();
+        if (Number(campaign_type_id)) {
+            $.ajax({
+                url: base_url + '/upload-lead/fetch-campaign',
+                type: 'POST',
+                data: { campaign_type_id: campaign_type_id },
+                success: function (res) {
+
+                    $('#campaign_id').empty();
+                    $('#campaign_id').attr('required', true);
+                    $('.campaign_star').text('*');
+                    $('#campaign_id').append(`<option value="" selected disabled>Select Campaign</option>`);
+                    res.forEach(campaign => {
+                        $('#campaign_id').append(`<option value="${campaign.id}">${campaign.name}</option>`);
+                    });
+                },
+                error: function (err) {
+                    console.error('Fetch failed:', err);
+                }
+            });
+        }
+    });
+    //fetch campaign on Campaign Type change end
+});
--- resources/views/maincontents/upload-lead/add-edit.blade.php
+++ resources/views/maincontents/upload-lead/add-edit.blade.php
@@ -4,7 +4,7 @@ $controllerRoute = $module['controller_route'];
 ?>
 @extends('layouts.main')
 @section('content')
- 
+
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="row g-6">
             <h4><?= $page_header ?></h4>
@@ -55,12 +55,14 @@ $controllerRoute = $module['controller_route'];
                                 <div class="col-md-6 mb-3">
                                     <label for="telecaller_id" class="form-label">Telecaller <small
                                             class="text-danger">*</small></label>
-                                    <select class="form-control" id="telecaller_id" name="telecaller_id[]"
+                                    <select class="form-control select2" id="telecaller_id" name="telecaller_id[]"
                                         required multiple>
                                         <option value="" selected disabled>Select Telecaller</option>                                       
 
                                     </select>
                                 </div>
+                                
+                                
                                 
 
                                 <div class="col-md-6 mb-3">
@@ -111,70 +113,6 @@ $controllerRoute = $module['controller_route'];
     </div>
 @endsection
 
-<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-    crossorigin="anonymous"></script>
-    
-
-<script>
-    $(document).ready(function() {
-        // fetch telecallers on branch change 
-        let branch_id;
-        $(document).on('change', '#branch_id', function() {
-            branch_id = $(this).val();
-            if (Number(branch_id)) {
-                $.ajax({
-                    url: "{{ url('/') }}/{{ $controllerRoute }}/fetch-telecaller",
-                    type: "POST",
-                    data: {
-                        branch_id: branch_id,
-                        _token: '{{ csrf_token() }}'
-                    },
-                    success: function(res) {
-
-                        $('#telecaller_id').empty();
-                        $('#telecaller_id').append(`
-                      <option value="" selected disabled>Select Telecaller</option>
-                    `);
-                        res.forEach(telecaller => {
-                            $('#telecaller_id').append(`
-                        <option value="${telecaller.id}">${telecaller.first_name} ${telecaller.last_name}</option>
-                    `);
-                        });
-
-                    },
-                    error: function(err) {
-                        console.error('Fetch failed:', err);
-                    },
-                });
-            }
-        });
-        // fetch telecallers on branch change end
-
-        //fetch campaign on Campaign Type change
-        let campaign_type_id;
-        $(document).on('change','#campaign_type_id', function(){
-            campaign_type_id = $(this).val();
-            if(Number(campaign_type_id)){
-                $.ajax({
-                     url: "{{ url('/') }}/{{ $controllerRoute }}/fetch-campaign",
-                    type: "POST",
-                    data: {campaign_type_id: campaign_type_id, _token: '{{ csrf_token() }}'},
-                    success: function(res){
-                        $('#campaign_id').empty();
-                        $('#campaign_id').attr('required', true);
-                        $('.campaign_star').text('*');
-                        $('#campaign_id').append(`<option value="" selected disabled>Select Campaign</option>`);
-                        res.forEach(campaign => {
-                            $('#campaign_id').append(`<option value="${campaign.id}">${campaign.name}</option>`);
-                        });
-                    },
-                    error: function(err) {
-                        console.error('Fetch failed:', err);
-                    }
-                });
-            }
-        });
-        //fetch campaign on Campaign Type change end
-    });
-    
-</script>
+@section('scripts')
+<script src="<?=config('constants.admin_assets_url')?>assets/js/upload-lead.js"></script>
+@endsection
