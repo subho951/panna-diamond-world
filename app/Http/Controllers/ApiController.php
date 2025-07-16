5ee401761e300a99ac9d9524c600abdd71e35d02
subhomoy samanta <subhomoy@keylines.net>
2025-07-16 11:59:06 +0530

    before login api implemented

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,229 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use App\Models\GeneralSetting;
+use App\Models\UserActivity;
+
+use App\Services\SiteAuthService;
+use App\Helpers\Helper;
+
+use Auth;
+use Session;
+use Hash;
+use DB;
+use App\Libraries\CreatorJwt;
+use App\Libraries\JWT;
+date_default_timezone_set("Asia/Calcutta");
+
+class ApiController extends Controller
+{
+    protected $siteAuthService;
+    protected $data;
+    
+    /* before login screen */
+        /* general settings */
+            public function getAppSetting(Request $request){
+                $apiStatus          = TRUE;
+                $apiMessage         = '';
+                $apiResponse        = [];
+                $apiExtraField      = '';
+                $apiExtraData       = '';
+                $requestData        = $request->all();
+                $requiredFields     = ['key', 'source'];
+                $headerData         = $request->header();
+                if (!$this->validateArray($requiredFields, $requestData)){
+                    $apiStatus          = FALSE;
+                    $apiMessage         = 'All Data Are Not Present !!!';
+                }
+                if($headerData['key'][0] == env('PROJECT_KEY')){
+                    $generalSetting = GeneralSetting::find(1);
+                    Helper::pr($generalSetting);
+                    if($generalSetting){
+                        $apiResponse = [
+                            'site_name'             => $generalSetting->site_name,
+                            'site_phone'            => $generalSetting->site_phone,
+                            'site_phone2'           => $generalSetting->site_phone2,
+                            'site_mail'             => $generalSetting->site_mail,
+                            'system_email'          => $generalSetting->system_email,
+                            'site_url'              => $generalSetting->site_url,
+                            'site_logo'             => env('UPLOADS_URL').$generalSetting->site_logo,
+                            'site_footer_logo'      => env('UPLOADS_URL').$generalSetting->site_footer_logo,
+                            'site_favicon'          => env('UPLOADS_URL').$generalSetting->site_favicon,
+                            'site_address'          => $generalSetting->description,
+                            'theme_color'           => $generalSetting->theme_color,
+                            'font_color'            => $generalSetting->font_color,
+                            'sidebar_bgcolor'       => $generalSetting->sidebar_bgcolor,
+                            'header_bgcolor'        => $generalSetting->header_bgcolor,
+                            'twitter_profile'       => $generalSetting->twitter_profile,
+                            'facebook_profile'      => $generalSetting->facebook_profile,
+                            'instagram_profile'     => $generalSetting->instagram_profile,
+                            'linkedin_profile'      => $generalSetting->linkedin_profile,
+                            'youtube_profile'       => $generalSetting->youtube_profile,
+                        ];
+                    }
+                    http_response_code(200);
+                    $apiStatus          = TRUE;
+                    $apiMessage         = 'Data Available !!!';
+                    $apiExtraField      = 'response_code';
+                    $apiExtraData       = http_response_code();
+                } else {
+                    http_response_code(200);
+                    $apiStatus          = FALSE;
+                    $apiMessage         = $this->getResponseCode(http_response_code());
+                    $apiExtraField      = 'response_code';
+                    $apiExtraData       = http_response_code();
+                }
+                $this->response_to_json($apiStatus, $apiMessage, $apiResponse, $apiExtraField, $apiExtraData);
+            }
+        /* general settings */
+    /* before login screen */
+    /* authentication */
+
+    /* authentication */
+    /* after login screen */
+
+    /* after login screen */
+
+    /*
+    Get http response code
+    Author : Subhomoy
+    */
+    private function getResponseCode($code = NULL){
+        if ($code !== NULL) {
+            switch ($code) {
+                case 100: $text = 'Continue'; break;
+                case 101: $text = 'Switching Protocols'; break;
+                case 200: $text = 'OK'; break;
+                case 201: $text = 'Created'; break;
+                case 202: $text = 'Accepted'; break;
+                case 203: $text = 'Non-Authoritative Information'; break;
+                case 204: $text = 'No Content'; break;
+                case 205: $text = 'Reset Content'; break;
+                case 206: $text = 'Partial Content'; break;
+                case 300: $text = 'Multiple Choices'; break;
+                case 301: $text = 'Moved Permanently'; break;
+                case 302: $text = 'Moved Temporarily'; break;
+                case 303: $text = 'See Other'; break;
+                case 304: $text = 'Not Modified'; break;
+                case 305: $text = 'Use Proxy'; break;
+                case 400: $text = 'Unauthenticated Request !!!'; break;
+                case 401: $text = 'Token Not Found !!!'; break;
+                case 402: $text = 'Payment Required'; break;
+                case 403: $text = 'Token Has Expired !!!'; break;
+                case 404: $text = 'User Not Found !!!'; break;
+                case 405: $text = 'Method Not Allowed'; break;
+                case 406: $text = 'All Data Are Not Present !!!'; break;
+                case 407: $text = 'Proxy Authentication Required'; break;
+                case 408: $text = 'Request Time-out'; break;
+                case 409: $text = 'Conflict'; break;
+                case 410: $text = 'Gone'; break;
+                case 411: $text = 'Length Required'; break;
+                case 412: $text = 'Precondition Failed'; break;
+                case 413: $text = 'Request Entity Too Large'; break;
+                case 414: $text = 'Request-URI Too Large'; break;
+                case 415: $text = 'Unsupported Media Type'; break;
+                case 500: $text = 'Internal Server Error'; break;
+                case 501: $text = 'Not Implemented'; break;
+                case 502: $text = 'Bad Gateway'; break;
+                case 503: $text = 'Service Unavailable'; break;
+                case 504: $text = 'Gateway Time-out'; break;
+                case 505: $text = 'HTTP Version not supported'; break;
+                default:
+                    exit('Unknown http status code "' . htmlentities($code) . '"');
+                break;
+            }
+            $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+            header($protocol . ' ' . $code . ' ' . $text);
+            $GLOBALS['http_response_code'] = $code;
+        } else {
+            $code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+            $text = '';
+        }
+        return $text;
+    }
+    /*
+    Generate JWT tokens for authentication
+    Author : Subhomoy
+    */
+    private static function generateToken($userId, $email, $phone){
+        $token      = array(
+            'id'                => $userId,
+            'email'             => $email,
+            'phone'             => $phone,
+            'exp'               => time() + (30 * 24 * 60 * 60) // 30 days
+        );
+        // pr($token);
+        return JWT::encode($token, TOKEN_SECRET, 'HS256');
+    }
+    /*
+    Check Authentication
+    Author : Subhomoy
+    */
+    private function tokenAuth($appAccessToken){
+        $headers = apache_request_headers();
+        if (isset($appAccessToken) && !empty($appAccessToken)) :
+            $userdata = $this->matchToken($appAccessToken);
+            // pr($userdata);
+            if ($userdata['status']) :
+                $checkToken =  UserDevice::where('user_id', '=', $userdata['data']->id)->where('app_access_token', '=', $appAccessToken)->first();
+                // echo $this->db->last_query();
+                // pr($userdata);
+                if (!empty($checkToken)) :
+                    if ($userdata['data']->exp && $userdata['data']->exp > time()) :
+                        $tokenStatus = array(TRUE, $userdata['data']->id, $userdata['data']->email, $userdata['data']->phone, $userdata['data']->exp);
+                    else :
+                        $tokenStatus = array(FALSE, 'Token Has Expired 1 !!!');
+                    endif;
+                else :
+                    $tokenStatus = array(FALSE, 'Token Has Expired 2 !!!');
+                endif;
+            else :
+                $tokenStatus = array(FALSE, 'Token Not Found !!!');
+            endif;
+        else :
+            $tokenStatus = array(FALSE, 'Token Not Found In Request !!!');
+        endif;
+        if ($tokenStatus[0]) :
+            $this->userId           = $tokenStatus[1];
+            $this->userEmail        = $tokenStatus[2];
+            $this->userMobile       = $tokenStatus[3];
+            $this->userExpiry       = $tokenStatus[4];
+            // pr($tokenStatus);
+            return array('status' => TRUE, 'data' => $tokenStatus);
+        else :
+            return array('status' => FALSE, 'data' => $tokenStatus[1]);
+            // $this->response_to_json(FALSE, $tokenStatus[1]);
+        endif;
+    }
+    /*
+    Match JWT token with user token saved in database
+    Author : Subhomoy
+    */
+    private static function matchToken($token){
+        // try{
+        //     // $decoded    = JWT::decode($token, TOKEN_SECRET, 'HS256');
+        //     $decoded    = JWT::decode($token, new Key(TOKEN_SECRET, 'HS256'));
+        //     // pr($decoded);
+        // } catch (\Exception $e) {
+        //     //echo 'Caught exception: ',  $e->getMessage(), "\n";
+        //     return array('status' => FALSE, 'data' => '');
+        // }
+        
+        // return array('status' => TRUE, 'data' => $decoded);
+
+
+        try{
+            $key = "1234567890qwertyuiopmnbvcxzasdfghjkl";
+            $decoded = JWT::decode($token, $key, array('HS256'));
+            // $decodedData = (array) $decoded;
+        } catch (\Exception $e) {
+            //echo 'Caught exception: ',  $e->getMessage(), "\n";
+            return array('status' => FALSE, 'data' => '');
+        }
+        return array('status' => TRUE, 'data' => $decoded);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
+Route::prefix('api')->namespace('App\Http\Controllers')->group(function(){
+    /* before login */
+        Route::match(['get'], '/get-app-setting', [ApiController::class, 'getAppSetting']);
+        Route::match(['post'], '/get-static-pages', [ApiController::class, 'getStaticPages']);
+
+        Route::match(['post'], '/signin', [ApiController::class, 'signin']);
+        Route::match(['post'], '/signin-with-mobile', [ApiController::class, 'signinWithMobile']);
+        Route::match(['post'], '/signin-validate-mobile', [ApiController::class, 'signinValidateMobile']);
+        Route::match(['post'], '/forgot-password', [ApiController::class, 'forgotPassword']);
+        Route::match(['post'], '/validate-otp', [ApiController::class, 'validateOtp']);
+        Route::match(['post'], '/resend-otp', [ApiController::class, 'resendOtp']);
+        Route::match(['post'], '/reset-password', [ApiController::class, 'resetPassword']);
+    /* before login */
+    /* after login */
+        Route::match(['get'], '/signout', [ApiController::class, 'signout']);
+        Route::match(['get'], '/dashboard', [ApiController::class, 'dashboard']);
+        Route::match(['post'], '/change-password', [ApiController::class, 'changePassword']);
+        Route::match(['get'], '/get-profile', [ApiController::class, 'getProfile']);
+        Route::match(['get'], '/edit-profile', [ApiController::class, 'editProfile']);
+        Route::match(['post'], '/upload-profile-image', [ApiController::class, 'uploadProfileImage']);
+        Route::match(['post'], '/update-profile', [ApiController::class, 'updateProfile']);
+        Route::match(['get'], '/delete-account', [ApiController::class, 'deleteAccount']);
+    /* after login */
+});
