ea60f90f27f10e1f1f56fafe25ebecff2cdc9d76
subhomoy samanta <subhomoy@keylines.net>
2025-07-24 11:18:40 +0530

    lead detail api implemented

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1544,7 +1544,7 @@ class ApiController extends Controller
                                 $getActivities      = LeadActivity::where('lead_sl_no', '=', $sl_no)->first();
                                 if($getActivities){
                                     foreach($getActivities as $getActivity){
-                                        echo $getActivity->purpose_id;
+                                        
                                         Helper::pr($getActivity);
                                         $getPurpose         = Purpose::select('name')->where('id', '=', $getActivity->purpose_id)->first();
                                         Helper::pr($getPurpose);
