9aff13c45393c3091828ee651e7cbebe9ebb441c
subhomoy samanta <subhomoy@keylines.net>
2025-07-29 11:02:04 +0530

    lead details api updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1727,6 +1727,96 @@ class ApiController extends Controller
                                 ];
                                 BranchLead::where('lead_sl_no', '=', $sl_no)->update($fields2);
 
+                                /* send lead details response in return */
+                                    $leadNo                  = BranchLead::select('lead_sl_no', 'parent_status_id', 'child_status_id', 'next_followup_date', 'next_followup_time', 'created_at', 'campaign_type_id', 'campaign_id')
+                                                        ->where('status', '=', 1)
+                                                        ->where('lead_sl_no', '=', $sl_no)
+                                                        ->first();
+
+                                    if($leadNo){
+                                        $activity_count = LeadActivity::where('lead_sl_no', '=', $sl_no)->count();
+                                        $last_activity  = LeadActivity::where('lead_sl_no', '=', $sl_no)->orderBy('id', 'DESC')->first();
+                                        $next_schedule  = '';
+                                        if($activity_count > 0){
+                                            if($leadNo->next_followup_date != '' && $leadNo->next_followup_time != ''){
+                                                $next_schedule = date_format(date_create($leadNo->next_followup_date), "M d, Y") . ', ' . date_format(date_create($leadNo->next_followup_time), "h:i A");
+                                            }
+                                        }
+                                        $getParentStatus    = LeadStatus::select('name')->where('id', '=', $leadNo->parent_status_id)->first();
+                                        $getChildStatus     = LeadStatus::select('name')->where('id', '=', $leadNo->child_status_id)->first();
+                                        $getMasterLead      = MasterLead::select('lead_no')->where('sl_no', '=', $sl_no)->first();
+                                        $getCampaignType    = CampaignType::select('name')->where('id', '=', $leadNo->campaign_type_id)->first();
+                                        $getCampaign        = Campaign::select('name')->where('id', '=', $leadNo->campaign_id)->first();
+
+                                        $activities         = [];
+                                        $getActivities      = LeadActivity::where('lead_sl_no', '=', $sl_no)->orderBy('id', 'DESC')->get();
+                                        if($getActivities){
+                                            foreach($getActivities as $getActivity){
+                                                $getPurpose         = Purpose::select('name')->where('id', '=', $getActivity->purpose_id)->first();
+                                                $getParentStatus    = LeadStatus::select('name')->where('id', '=', $getActivity->parent_status_id)->first();
+                                                $getChildStatus     = LeadStatus::select('name')->where('id', '=', $getActivity->child_status_id)->first();
+                                                $getTelecaller      = User::select('first_name', 'last_name')->where('id', '=', $getActivity->assigned_telecaller_id)->first();
+                                                $getMood            = Mood::select('name', 'emoji', 'color')->where('id', '=', $getActivity->mood)->first();
+
+                                                $next_schedule_activity  = '';
+                                                if($getActivity->next_followup_date != '' && $getActivity->next_followup_time != ''){
+                                                    $next_schedule_activity = date_format(date_create($getActivity->next_followup_date), "M d, Y") . ', ' . date_format(date_create($getActivity->next_followup_time), "h:i A");
+                                                }
+
+                                                $feedback_tags = [];
+                                                if($getActivity->feedback_tag_ids != ''){
+                                                    $feedback_tag_ids  = json_decode($getActivity->feedback_tag_ids);
+                                                    if(!empty($feedback_tag_ids)){
+                                                        for($f=0;$f<count($feedback_tag_ids);$f++){
+                                                            $getTag             = FeedbackTag::select('name')->where('id', '=', $feedback_tag_ids[$f])->first();
+                                                            $feedback_tags[]    = (($getTag)?$getTag->name:'');
+                                                        }
+                                                    }
+                                                }
+
+                                                $activities[]         = [
+                                                    'purpose_name'          => (($getPurpose)?$getPurpose->name:''),
+                                                    'comment'               => $getActivity->comment,
+                                                    // 'note'                  => $getActivity->note,
+                                                    'telecaller_name'       => (($getTelecaller)?$getTelecaller->first_name . ' ' . $getTelecaller->last_name:''),
+                                                    'parent_status_id'      => $getActivity->parent_status_id,
+                                                    'parent_status_name'    => (($getParentStatus)?$getParentStatus->name:''),
+                                                    'child_status_id'       => $getActivity->child_status_id,
+                                                    'child_status_name'     => (($getChildStatus)?$getChildStatus->name:''),
+                                                    'activity_timestamp'    => date_format(date_create($getActivity->created_at), "M d, Y h:i A"),
+                                                    'next_schedule'         => $next_schedule_activity,
+                                                    'feedback_tags'         => $feedback_tags,
+                                                    'mood_id'               => $getActivity->mood,
+                                                    'mood_name'             => (($getMood)?$getMood->name:''),
+                                                    'mood_emoji'            => (($getMood)?$getMood->emoji:''),
+                                                    'mood_color'            => (($getMood)?$getMood->color:''),
+                                                ];
+                                            }
+                                        }
+
+                                        $apiResponse        = [
+                                            'sl_no'                 => $sl_no,
+                                            'lead_no'               => (($getMasterLead)?$getMasterLead->lead_no:''),
+                                            'company_name'          => $this->getHeaderValueByID($sl_no, 1),
+                                            'contact_person_name'   => $this->getHeaderValueByID($sl_no, 2),
+                                            'email'                 => $this->getHeaderValueByID($sl_no, 5),
+                                            'phone_no'              => $this->getHeaderValueByID($sl_no, 4),
+                                            'whatsapp_no'           => $this->getHeaderValueByID($sl_no, 14),
+                                            'parent_status_id'      => (($leadNo->parent_status_id > 0)?$leadNo->parent_status_id:12),
+                                            'parent_status_name'    => (($getParentStatus)?$getParentStatus->name:'New'),
+                                            'child_status_id'       => (($leadNo->child_status_id > 0)?$leadNo->child_status_id:13),
+                                            'child_status_name'     => (($getChildStatus)?$getChildStatus->name:'New'),
+                                            'campaign_type_name'    => (($getCampaignType)?$getCampaignType->name:''),
+                                            'campaign_name'         => (($getCampaign)?$getCampaign->name:''),
+                                            'last_call'             => (($activity_count > 0)?date_format(date_create($last_activity->created_at), "M d Y, h:i a"):''),
+                                            'next_schedule'         => $next_schedule,
+                                            'telecaller_name'       => $getUser->first_name . ' ' . $getUser->last_name,
+                                            'activity_count'        => $activity_count,
+                                            'activities'            => $activities,
+                                        ];
+                                    }
+                                /* send lead details response in return */
+
                                 $apiStatus          = TRUE;
                                 http_response_code(200);
                                 $apiMessage         = 'Lead Status Updated Successfully !!!';
