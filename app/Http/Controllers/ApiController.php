b98300f7f099247faa65ed4df6c736ad8b8c7db7
subhomoy samanta <subhomoy@keylines.net>
2025-07-30 14:48:21 +0530

    dashboard api implemented

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1173,6 +1173,84 @@ class ApiController extends Controller
         /* delete account */
     /* after login screen */
     /* after login lead */
+        /* dashboard */
+            public function dashboard(Request $request)
+            {
+                $apiStatus          = TRUE;
+                $apiMessage         = '';
+                $apiResponse        = [];
+                $apiExtraField      = '';
+                $apiExtraData       = '';
+                $requestData        = $request->all();
+                $requiredFields     = ['key', 'source'];
+                $headerData         = $request->header();
+                if (!$this->validateArray($requiredFields, $requestData)){
+                    $apiStatus          = FALSE;
+                    $apiMessage         = 'All Data Are Not Present !!!';
+                }
+                if($headerData['key'][0] == env('PROJECT_KEY')){
+                    $app_access_token           = $headerData['authorization'][0];
+                    $getTokenValue              = $this->tokenAuth($app_access_token);
+                    if($getTokenValue['status']){
+                        $uId        = $getTokenValue['data'][1];
+                        $expiry     = date('d/m/Y H:i:s', $getTokenValue['data'][4]);
+                        $getUser    = User::where('id', '=', $uId)->first();
+                        if($getUser){
+                            $getParentStats = LeadStatus::select('id', 'name', 'background_color', 'font_color')->where('status', '=', 1)->where('parent_id', '=', 0)->orderBy('rank', 'ASC')->get();
+                            if($getParentStats){
+                                foreach($getParentStats as $getParentStat){
+                                    $child_status = [];
+                                    $getChildStats = LeadStatus::select('id', 'name', 'background_color', 'font_color')->where('status', '=', 1)->where('parent_id', '=', $getParentStat->id)->orderBy('rank', 'ASC')->get();
+                                    if($getChildStats){
+                                        foreach($getChildStats as $getChildStat){
+                                            $child_status[]            = [
+                                                'child_status_id'                  => $getChildStat->id,
+                                                'child_status_name'                => $getChildStat->name,
+                                                'child_status_background_color'    => $getChildStat->background_color,
+                                                'child_status_font_color'          => $getChildStat->font_color,
+                                            ];
+                                        }
+                                    }
+
+                                    $apiResponse[]            = [
+                                        'parent_status_id'                  => $getParentStat->id,
+                                        'parent_status_name'                => $getParentStat->name,
+                                        'parent_status_background_color'    => $getParentStat->background_color,
+                                        'parent_status_font_color'          => $getParentStat->font_color,
+                                        'child_status'                      => $child_status,
+                                    ];
+                                }
+                            }
+
+                            $apiStatus          = TRUE;
+                            http_response_code(200);
+                            $apiMessage         = 'Data Available !!!';
+                            $apiExtraField      = 'response_code';
+                            $apiExtraData       = http_response_code();
+                        } else {
+                            $apiStatus          = FALSE;
+                            http_response_code(200);
+                            $apiMessage         = 'User Not Found !!!';
+                            $apiExtraField      = 'response_code';
+                            $apiExtraData       = http_response_code();
+                        }
+                    } else {
+                        http_response_code(200);
+                        $apiExtraField      = 'response_code';
+                        $apiExtraData       = http_response_code();
+                        $apiStatus          = FALSE;
+                        $apiMessage         = 'Something Went Wrong !!!';
+                    }                                               
+                } else {
+                    http_response_code(200);
+                    $apiStatus          = FALSE;
+                    $apiMessage         = $this->getResponseCode(http_response_code());
+                    $apiExtraField      = 'response_code';
+                    $apiExtraData       = http_response_code();
+                }
+                $this->response_to_json($apiStatus, $apiMessage, $apiResponse, $apiExtraField, $apiExtraData);
+            }
+        /* dashboard */
         /* get lead status */
             public function getLeadStatusList(Request $request)
             {
