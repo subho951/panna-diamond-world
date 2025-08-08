79caa99fb694c55a6b76248f2a0a094b190038ea
subhomoy samanta <subhomoy@keylines.net>
2025-08-08 17:09:26 +0530

    static page url create

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ use App\Http\Controllers\PurposeController;
 
 // GET route – to display the page
 Route::get('/test-email-function', [AuthController::class, 'showEmailTestPage']);
-Route::get('/page/{id}', [UserController::class, 'page']);
+Route::get('/page-content/{id}', [UserController::class, 'page']);
 
 // POST route – to send the email
 Route::post('/test-email-function', [AuthController::class, 'testEmailFunction']);
