437b467f71fe89d7c753dcfdbaf4022d3d371082
subhomoy samanta <subhomoy@keylines.net>
2025-08-08 17:18:40 +0530

    static page url create

--- resources/views/maincontents/delete-account-request.blade.php
+++ resources/views/maincontents/delete-account-request.blade.php
@@ -9,7 +9,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <h3 class="mb-3">Delete Account Request</h3>
+            <h3 class="mb-3" style="text-align: center;">Delete Account Request</h3>
             <div class="col-md-12">
                 @if(session('success_message'))
                 <div class="alert alert-success">
