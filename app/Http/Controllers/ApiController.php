90771949f7128e7a271ae31b3eb28c50ffaac367
subhomoy samanta <subhomoy@keylines.net>
2025-08-15 13:20:41 +0530

    client feedback points update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1740,6 +1740,147 @@ class ApiController extends Controller
                 $this->response_to_json($apiStatus, $apiMessage, $apiResponse, $apiExtraField, $apiExtraData);
             }
         /* lead list */
+        /* lead list by dob & anniversary date */
+            public function leadListByDobAnni(Request $request)
+            {
+                $apiStatus          = TRUE;
+                $apiMessage         = '';
+                $apiResponse        = [];
+                $apiExtraField      = '';
+                $apiExtraData       = '';
+                $requestData        = $request->all();
+                $requiredFields     = ['key', 'source', 'page_no', 'per_page', 'date_type'];
+                $headerData         = $request->header();
+                if (!$this->validateArray($requiredFields, $requestData)){
+                    $apiStatus          = FALSE;
+                    $apiMessage         = 'All Data Are Not Present !!!';
+                }
+                if($headerData['key'][0] == env('PROJECT_KEY')){
+                    $app_access_token           = $headerData['authorization'][0];
+                    $getTokenValue              = $this->tokenAuth($app_access_token);
+                    if($getTokenValue['status']){
+                        $uId                    = $getTokenValue['data'][1];
+                        $expiry                 = date('d/m/Y H:i:s', $getTokenValue['data'][4]);
+                        $getUser                = User::where('id', '=', $uId)->first();
+
+                        $page_no                = $requestData['page_no'];
+                        $per_page               = $requestData['per_page'];
+                        $date_type              = $requestData['date_type'];
+                        if($getUser){
+                            $branch_id                      = $getUser->branch_id;
+                            $assigned_telecaller_id         = $uId;
+                            $limit                          = $per_page; // per page elements
+                            if($page_no == 1){
+                                $offset         = 0;
+                            } else {
+                                $offset         = (($limit * $page_no) - $limit); // ((15 * 3) - 15)
+                            }
+
+                            $current_date       = date('d-m');
+                            if($date_type == 'dob'){
+                                $leadNos = DB::table('branch_leads')
+                                                                        ->join('master_leads', 'branch_leads.lead_sl_no', '=', 'master_leads.sl_no')
+                                                                        ->select('branch_leads.lead_sl_no', 'branch_leads.parent_status_id', 'branch_leads.child_status_id', 'branch_leads.next_followup_date', 'branch_leads.next_followup_time', 'branch_leads.created_at', 'branch_leads.campaign_type_id', 'branch_leads.campaign_id')
+                                                                        ->where('master_leads.header_id', '=', 12)
+                                                                        ->where('master_leads.header_value', 'LIKE', '%' . $current_date . '%')
+                                                                        ->where('branch_leads.assigned_telecaller_id', '=', $assigned_telecaller_id)
+                                                                        ->orderBy('branch_leads.lead_sl_no', 'ASC')
+                                                                        ->offset($offset)
+                                                                        ->limit($limit)
+                                                                        ->get();
+                            }
+                            if($date_type == 'anni'){
+                                $leadNos = DB::table('branch_leads')
+                                                                        ->join('master_leads', 'branch_leads.lead_sl_no', '=', 'master_leads.sl_no')
+                                                                        ->select('branch_leads.lead_sl_no', 'branch_leads.parent_status_id', 'branch_leads.child_status_id', 'branch_leads.next_followup_date', 'branch_leads.next_followup_time', 'branch_leads.created_at', 'branch_leads.campaign_type_id', 'branch_leads.campaign_id')
+                                                                        ->where('master_leads.header_id', '=', 13)
+                                                                        ->where('master_leads.header_value', 'LIKE', '%' . $current_date . '%')
+                                                                        ->where('branch_leads.assigned_telecaller_id', '=', $assigned_telecaller_id)
+                                                                        ->orderBy('branch_leads.lead_sl_no', 'ASC')
+                                                                        ->offset($offset)
+                                                                        ->limit($limit)
+                                                                        ->get();
+                            }
+                            // $leadNos                  = BranchLead::select('lead_sl_no', 'parent_status_id', 'child_status_id', 'next_followup_date', 'next_followup_time', 'created_at', 'campaign_type_id', 'campaign_id')
+                            //                             ->where('status', '=', 1)
+                            //                             ->where('branch_id', '=', $branch_id)
+                            //                             ->where('assigned_telecaller_id', '=', $assigned_telecaller_id)
+                            //                             ->where('parent_status_id', '=', 0)
+                            //                             ->where('child_status_id', '=', 0)
+                            //                             ->orderBy('lead_sl_no', 'ASC')
+                            //                             ->offset($offset)
+                            //                             ->limit($limit)
+                            //                             ->get();
+
+                            if($leadNos){
+                                foreach($leadNos as $leadNo){
+                                    $activity_count = LeadActivity::where('lead_sl_no', '=', $leadNo->lead_sl_no)->count();
+                                    $last_activity  = LeadActivity::where('lead_sl_no', '=', $leadNo->lead_sl_no)->orderBy('id', 'DESC')->first();
+                                    $next_schedule  = '';
+                                    if($activity_count > 0){
+                                        if($leadNo->next_followup_date != '' && $leadNo->next_followup_time != ''){
+                                            $next_schedule = date_format(date_create($leadNo->next_followup_date), "M d Y") . ', ' . date_format(date_create($leadNo->next_followup_time), "h:i a");
+                                        }
+                                    }
+                                    $getParentStatus    = LeadStatus::select('name')->where('id', '=', $leadNo->parent_status_id)->first();
+                                    $getChildStatus     = LeadStatus::select('name')->where('id', '=', $leadNo->child_status_id)->first();
+                                    $getMasterLead      = MasterLead::select('lead_no')->where('sl_no', '=', $leadNo->lead_sl_no)->first();
+                                    $getCampaignType    = CampaignType::select('name')->where('id', '=', $leadNo->campaign_type_id)->first();
+                                    $getCampaign        = Campaign::select('name')->where('id', '=', $leadNo->campaign_id)->first();
+
+                                    $apiResponse[]      = [
+                                        'sl_no'                 => $leadNo->lead_sl_no,
+                                        'lead_no'               => (($getMasterLead)?$getMasterLead->lead_no:''),
+                                        'company_name'          => $this->getHeaderValueByID($leadNo->lead_sl_no, 1),
+                                        'contact_person_name'   => $this->getHeaderValueByID($leadNo->lead_sl_no, 2),
+                                        'email'                 => $this->getHeaderValueByID($leadNo->lead_sl_no, 5),
+                                        'phone_no'              => $this->getHeaderValueByID($leadNo->lead_sl_no, 4),
+                                        'whatsapp_no'           => $this->getHeaderValueByID($leadNo->lead_sl_no, 14),
+                                        'is_vip'                => (int) $this->getHeaderValueByID($leadNo->lead_sl_no, 17),
+                                        'is_purchased'          => (int) $this->getHeaderValueByID($leadNo->lead_sl_no, 18),
+                                        'parent_status_id'      => (($leadNo->parent_status_id > 0)?$leadNo->parent_status_id:12),
+                                        'parent_status_name'    => (($getParentStatus)?$getParentStatus->name:'New'),
+                                        'child_status_id'       => (($leadNo->child_status_id > 0)?$leadNo->child_status_id:13),
+                                        'child_status_name'     => (($getChildStatus)?$getChildStatus->name:'New'),
+                                        'campaign_type_name'    => (($getCampaignType)?$getCampaignType->name:''),
+                                        'campaign_name'         => (($getCampaign)?$getCampaign->name:''),
+                                        'last_call'             => (($activity_count > 0)?date_format(date_create($last_activity->created_at), "M d Y, h:i a"):''),
+                                        'next_schedule'         => $next_schedule,
+                                        'activity_count'        => $activity_count,
+                                        'telecaller_name'       => $getUser->first_name . ' ' . $getUser->last_name,
+                                    ];
+                                }
+                            }
+
+                            $apiStatus          = TRUE;
+                            http_response_code(200);
+                            $apiMessage         = 'Data Available !!!';
+                            $apiExtraField      = 'response_code';
+                            $apiExtraData       = http_response_code();
+                        } else {
+                            $apiStatus          = FALSE;
+                            http_response_code(200);
+                            $apiMessage         = 'User Not Found !!!';
+                            $apiExtraField      = 'response_code';
+                            $apiExtraData       = http_response_code();
+                        }
+                    } else {
+                        http_response_code(200);
+                        $apiExtraField      = 'response_code';
+                        $apiExtraData       = http_response_code();
+                        $apiStatus          = FALSE;
+                        $apiMessage         = 'Something Went Wrong !!!';
+                    }                                               
+                } else {
+                    http_response_code(200);
+                    $apiStatus          = FALSE;
+                    $apiMessage         = $this->getResponseCode(http_response_code());
+                    $apiExtraField      = 'response_code';
+                    $apiExtraData       = http_response_code();
+                }
+                $this->response_to_json($apiStatus, $apiMessage, $apiResponse, $apiExtraField, $apiExtraData);
+            }
+        /* lead list by dob & anniversary date */
         /* lead details */
             public function leadDetail(Request $request)
             {
--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ use App\Http\Controllers\ApiController;
     Route::match(['get'], '/get-lead-status-list', [ApiController::class, 'getLeadStatusList']);
     Route::match(['get'], '/get-followup-option', [ApiController::class, 'getFollowupOption']);
     Route::match(['post'], '/lead-list', [ApiController::class, 'leadList']);
+    Route::match(['post'], '/lead-list-by-dob-anni', [ApiController::class, 'leadListByDobAnni']);
     Route::match(['post'], '/lead-details', [ApiController::class, 'leadDetail']);
     Route::match(['post'], '/update-lead-status', [ApiController::class, 'updateLeadStatus']);
     Route::match(['post'], '/update-lead-info', [ApiController::class, 'updateLeadInfo']);
