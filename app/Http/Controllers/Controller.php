7ac1c1be6506f4d487bb56f40d632a31a61a306b
subhomoy samanta <subhomoy@keylines.net>
2025-09-04 15:03:43 +0530

    brevo email set

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -25,14 +25,14 @@ abstract class Controller
         $mailLibrary->CharSet       = 'UTF-8';
         $mailLibrary->SMTPDebug     = 0;
         $mailLibrary->IsSMTP();
-        $mailLibrary->Host          = $generalSetting->smtp_host;
+        $mailLibrary->Host          = Helper::getSettingValue('smtp_host');
         $mailLibrary->SMTPAuth      = true;
-        $mailLibrary->Port          = $generalSetting->smtp_port;
-        $mailLibrary->Username      = $generalSetting->smtp_username;
-        $mailLibrary->Password      = $generalSetting->smtp_password;
+        $mailLibrary->Port          = Helper::getSettingValue('smtp_port');
+        $mailLibrary->Username      = Helper::getSettingValue('smtp_username');
+        $mailLibrary->Password      = Helper::getSettingValue('smtp_password');
         $mailLibrary->SMTPSecure    = 'smtp';
-        $mailLibrary->From          = $generalSetting->from_email;
-        $mailLibrary->FromName      = $generalSetting->from_name;
+        $mailLibrary->From          = Helper::getSettingValue('from_email');
+        $mailLibrary->FromName      = Helper::getSettingValue('from_name');
         // $mailLibrary->AddReplyTo($generalSetting->from_email, $generalSetting->from_name);
         if(is_array($email)) :
             foreach($email as $eml):
